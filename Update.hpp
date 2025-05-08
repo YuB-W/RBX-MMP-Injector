a7dcf68ba501f489ee463b2a8d0c6c510c5d7bfa
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-05-08 08:58:59 +0200

    Update Update.hpp

--- Update.hpp
+++ Update.hpp
@@ -1,12 +1,9 @@
-﻿#pragma once
+#pragma once
 #include "Windows.h"
 #include <cstdint>
 
 
-//  Roblox – Hyperion build 628 (April-30-2025) 
-//updated offsets for version-8328c0dc77d84a44
-
-
+//updated offsets for version-ff05edc617954c5b
 
 namespace ofssetss_new
 {
@@ -18,8 +15,8 @@ namespace ofssetss_new
 
 
     // whitelist stuff
-    static constexpr auto set_insert = 0xb57060;  // set_insert function
-    static constexpr auto whitelist_page = 0x2a86a0;  // whitelist function (same as set_insert now)
-    static constexpr auto Bitmap = 0x2A8558;  // whitelist function (same as set_insert now)
+    static constexpr auto set_insert = 0xe5c390;  // set_insert function 0xba15b0
+    static constexpr auto whitelist_page = 0x29d1d0;  // whitelist function (same as set_insert now) 0x2856b8
+    static constexpr auto Bitmap = 0x283ac0;  // whitelist function (same as set_insert now) 0x299ac0
 
 }
