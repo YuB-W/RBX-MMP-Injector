4aea67ca3a087738e6d15b30fb9c4156896b1bea
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-05-29 08:31:18 +0300

    Update Update.hpp

--- Update.hpp
+++ Update.hpp
@@ -2,9 +2,6 @@
 #include "Windows.h"
 #include <cstdint>
 
-
-//updated offsets for version-e00a4ca39fb04359
-
 namespace ofssetss_new
 {
     constexpr uint16_t  SCF_INSERTED_JMP = 0x04EB;
@@ -14,8 +11,8 @@ namespace ofssetss_new
 
 
     // whitelist stuff // need to update every roblox update!!!!!!
-    static constexpr auto set_insert = 0xE15010;
-    static constexpr auto whitelist_page = 0x2730A8;
-    static constexpr auto Bitmap = 0x2750F8;
+   static constexpr auto set_insert = 0xB86550;  
+   static constexpr auto whitelist_page = 0x286640;  
+   static constexpr auto Bitmap = 0x282f68; 
 
 }
